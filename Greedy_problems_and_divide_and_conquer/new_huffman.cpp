bec46b191f4d44575ac7d32476e15ae85ff22f82
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2024-04-26 12:01:35 +0530

    updated huffman encoding

--- Greedy_problems_and_divide_and_conquer/new_huffman.cpp
+++ Greedy_problems_and_divide_and_conquer/new_huffman.cpp
@@ -1,94 +1,84 @@
-/*
-    1- an unordered map stores characters and corresponding frequencies and sends it to huffman function.
-    2- node structure defines node of a huffman tree
-    3- a custom magnetic comparator is used to compare nodes in the priority queue .
+/* Huffman Encoding :
+1 functions : huffman encoding-builds tree, print code words recursively
+2 data structures : a struct node for tree , minheap for tree, 
+    custom comparator to build minheap according to frequencies of node
+    unordered map to insert characters and corresponding frequencies
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// Node of huffman tree
-struct Node
-{
-    char ch;
+struct Node{
+    char data;
     int freq;
-    Node *left, *right;
-
-    Node(char ch, int f)
-    {
-        this->ch = ch;
-        this->freq = f;
-        left = right = nullptr;
-    }
+    Node *left,*right;
+    Node(char data,int freq){
+        this->data=data;
+        this->freq=freq;
+        right=left=nullptr;
+    }  
 };
 
-struct compare
-{
-    bool operator()(Node *l, Node *r)
-    {
+struct compare{
+    bool operator()(Node *l,Node *r){
         return (l->freq > r->freq);
     }
 };
 
-void printCodes(Node *root, string code)
-{
-    if (root == nullptr)
+void printCodes(Node*root,string code){
+    if(root==nullptr)
         return;
-    if (root->ch != '\0')
-        cout << root->ch << " : " << code << endl;
-
-    printCodes(root->left, code + "0");
-    printCodes(root->right, code + "1");
+    if(root->data!='$')
+        cout<<root->data<<" : "<<code<<endl;
+    printCodes(root->left,code+"0");
+    printCodes(root->right,code+"1");
 }
 
-void Huffman_encoding(unordered_map<char, int> frequency)
-{
 
-    // initialization
-    priority_queue<Node *, vector<Node *>, compare> minHeap;
-    Node *left, *right, *newTop;
-
-    // filling the pq :
-    for (auto &it : frequency)
-    {
-        minHeap.push(new Node(it.first, it.second));
-    }
-
-    // building the huffman tree
-    while (minHeap.size() > 1)
-    {
-
-        // extract top 2 elements
-        left = minHeap.top();
-        minHeap.pop();
-
-        right = minHeap.top();
-        minHeap.pop();
-
-        newTop = new Node('\0', (left->freq + right->freq));
-        newTop->left = left;
-        newTop->right = right;
-
-        minHeap.push(newTop);
-    }
-
-    printCodes(minHeap.top(), "");
+void printTree(Node*root,bool left){
+    if(root==nullptr)
+        return;
+    if(left)
+        cout<<root->data<<"\t"<<endl;
+    else
+        cout<<"\t"<<root->data<<endl;
+    printTree(root->left,true);
+    printTree(root->right,false);
 }
 
-int main()
-{
-    unordered_map<char, int> frequency;
-    int n;
-    cout << "Enter number of charcaters :\n";
-    cin >> n;
-    int f;
-    char c;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter the character and frequency : ";
-        cin >> c >> f;
-        frequency[c] = f;
+void Huffman_encoding(unordered_map<char,int> &frequencies){
+    //initialise Priority queue
+    priority_queue<Node*,vector<Node*>,compare> minheap;
+    for(auto entry:frequencies){
+        minheap.push(new Node(entry.first,entry.second));
     }
-    Huffman_encoding(frequency);
+
+    while(minheap.size()!=1){
+        Node *left =minheap.top();
+        minheap.pop();
+
+        Node *right =minheap.top();
+        minheap.pop();
+
+        Node *root=new Node('$',(left->freq + right->freq)) ; //new root node
+        root->left =left;
+        root->right=right;
+        minheap.push(root);
+    } 
+    //printing code words :
+    cout<<"Code words are :\n";
+    printCodes(minheap.top(),"");
+    cout<<"Tree :\n";
+    printTree(minheap.top(),false);
+}
+int main(){
+    unordered_map<char,int> frequencies;
+    int n,freq;char ch;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>ch>>freq;
+        frequencies.insert({ch,freq});
+    }
+    Huffman_encoding(frequencies);
     return 0;
 }
