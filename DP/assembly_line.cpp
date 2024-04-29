35c78f179226bf67c93c6bd30bd40de8146cd8b5
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2024-04-29 17:52:56 +0530

    modified assembly line

--- DP/assembly_line.cpp
+++ DP/assembly_line.cpp
@@ -1,51 +1,83 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*Assembly line scheduling
+1- station time , entry exit time, station change time,dp of time, dp of line
+*/
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int carAssembly(vector<vector<int>>& a, vector<vector<int>>& t, vector<int>& e, vector<int>& x, int n) {
-    vector<int> dp1(n), dp2(n);
+void assembly_line(vector<vector<int>> &stn,vector<vector<int>> &t,vector<int> &e,vector<int> &x,int n){
 
-    // Base case
-    dp1[0] = e[0] + a[0][0]; // Time taken to reach station 1 on line 1
-    dp2[0] = e[1] + a[0][1]; // Time taken to reach station 1 on line 2
+    //initialising dp arrays;
+    vector<vector<int>> dp(2,vector<int>(n));
+    vector<vector<int>> line(2,vector<int>(n));
+    dp[0][0] = e[0] +stn[0][0];
+    dp[1][0] = e[1] +stn[1][0];
+    line[0][0]=1;
+    line[1][0]=2;
 
-    // Fill dp1 and dp2 arrays
-    for (int i = 1; i < n; ++i) {
-        dp1[i] = min(dp1[i - 1] + a[i][0], dp2[i - 1] + t[i - 1][1] + a[i][0]);
-        dp2[i] = min(dp2[i - 1] + a[i][1], dp1[i - 1] + t[i - 1][0] + a[i][1]);
+    int t11,t12,t21,t22,t_f1,t_f2,tf,lf;
+
+    for(int i=1;i<n;i++){
+
+        t11=dp[0][i-1]+stn[0][i];
+        t12=dp[1][i-1]+t[1][i-1]+stn[0][i];
+        t21=dp[0][i-1]+t[0][i-1]+stn[1][i];
+        t22=dp[1][i-1]+stn[1][i];
+
+        if(t11<t12){
+            dp[0][i]=t11;
+            line[0][i]=1;
+        }
+        else{
+            dp[0][i]=t12;
+            line[0][i]=2;
+        }
+
+        if(t21<t22){
+            dp[1][i]=t21;
+            line[1][i]=1;
+        }
+        else{
+            dp[1][i]=t22;
+            line[1][i]=2;
+        }
     }
+    t_f1=dp[0][n-1] + x[0];
+    t_f2=dp[1][n-1] + x[1];
 
-    // Return the minimum time to exit
-    return min(dp1[n - 1] + x[0], dp2[n - 1] + x[1]);
+    if(t_f1<t_f2){
+        tf=t_f1;
+        lf=1;
+    }
+    else{
+        tf=t_f2;
+        lf=2;
+    }
+    cout<<"The assembly comes out of the process via line "<<lf<<" in "<<tf<<" minutes.\n";
+    cout<<"Assembly path :\n";
+    string path = to_string(lf);
+    int j=(lf-1);
+    for(int i=(n-1);i>=0;i--){
+        path+=(" >-- "+to_string(line[j][i]));
+        j=(line[j][i]-1);
+    }
+    reverse(path.begin(),path.end());
+    cout<<path<<endl;
 }
 
-int main() {
-    int n; // Number of stations on each assembly line
-    cout << "Enter the number of stations: ";
-    cin >> n;
-
-    vector<vector<int>> a(n, vector<int>(2)); // Time taken at each station on each line
-    cout << "Enter the time taken at each station on line 1 and line 2: ";
-    for (int i = 0; i < n; ++i)
-        cin >> a[i][0] >> a[i][1];
-
-    vector<vector<int>> t(n - 1, vector<int>(2)); // Time taken to switch lines
-    cout << "Enter the time taken to switch lines: ";
-    for (int i = 0; i < n - 1; ++i)
-        cin >> t[i][0] >> t[i][1];
-
-    vector<int> e(2); // Entry time for each line
-    cout << "Enter entry time for line 1 and line 2: ";
-    cin >> e[0] >> e[1];
-
-    vector<int> x(2); // Exit time for each line
-    cout << "Enter exit time for line 1 and line 2: ";
-    cin >> x[0] >> x[1];
-
-    int minTime = carAssembly(a, t, e, x, n);
-    cout << "Minimum possible time by the car chassis to complete: " << minTime << endl;
-
+int main(){
+    int n;//no of stations
+    cin>>n;
+    vector<vector<int>> stn(2,vector<int>(n));
+    vector<vector<int>> t(2,vector<int>(n-1));
+    vector<int> e(2);
+    vector<int> x(2);
+    for(int i=0;i<n;i++)
+        cin>>stn[0][i]>>stn[1][i];
+    for(int i=0;i<n-1;i++)
+        cin>>t[0][i]>>t[1][i];
+    cin>>e[0]>>e[1];
+    cin>>x[0]>>x[1];
+    assembly_line(stn,t,e,x,n);
     return 0;
 }
